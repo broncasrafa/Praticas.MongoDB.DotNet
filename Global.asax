1d3ab94e808bfc4546fa9901bb5c23501c8addf7
rafael francisco <broncasrafa@gmail.com>
2018-04-23 16:39:27 -0300

    adding project

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MongoDB.DotNet.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MongoDB.DotNet.Startup))]
+namespace MongoDB.DotNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
